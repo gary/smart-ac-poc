b5ff37d45965a91dea622f52b0517c76f3cb4d72
Gary Iams <ge.iams@gmail.com>
2019-08-28 16:56:37 -0400

    Build endpoint for registering a new device

--- app/api/devices.rb
+++ app/api/devices.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
+# TODO: Document public API
+# :nodoc:
 class Devices < Grape::API
+  format :json
+  prefix :api
+
+  resource :devices do
+    params do
+      requires :serial_number, type: String, desc: 'Serial number'
+      optional :registered_at, type: DateTime, desc: 'Registration date'
+      requires :firmware_version, type: String, desc: 'Firmware version'
+    end
+    post '/register' do
+      Device.create!(serial_number: params[:serial_number],
+                     registered_at: params[:registered_at],
+                     firmware_version: params[:firmware_version])
+    end
+  end
 end
--- spec/requests/devices_spec.rb
+++ spec/requests/devices_spec.rb
@@ -3,11 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Devices do
-  describe 'GET /devices' do
-    it 'returns an HTTP 200' do
-      get '/devices'
+  describe 'POST /api/devices/register' do
+    it 'creates a new device' do
+      post('/api/devices/register', params: {
+             serial_number: '1',
+             registered_at: '2019-08-28',
+             firmware_version: '0'
+           })
 
-      expect(response.status).to eq(200)
+      expect(response.content_type).to eq('application/json')
+      expect(response).to have_http_status(:created)
     end
   end
 end
